6dfc188bf45e937f4d880f022f935260e3f3e20e
DMWatch-Updater <redzguilt@gmail.com>
2023-03-24 21:18:39 -0400

    Add more colors

--- src/main/java/com/DMWatch/ui/PlayerBanner.java
+++ src/main/java/com/DMWatch/ui/PlayerBanner.java
@@ -172,27 +172,27 @@ public class PlayerBanner extends JPanel
 		switch (player.getStatus())
 		{
 			case "0":
-				this.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.GREEN, 1)); break;
 			case "1":
-				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.GREEN, 1)); break;
 			case "2":
-				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 4)); break;
 			case "3":
-				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.RED, 4)); break;
 			case "4":
 				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
 			case "5":
-				this.setBorder(BorderFactory.createLineBorder(Color.RED, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.CYAN, 1)); break;
 			case "6":
-				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1)); break;
 			case "7":
-				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1)); break;
 			case "8":
-				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1)); break;
 			case "9":
-				this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1)); break;
 			default:
-				this.setBorder(BorderFactory.createLineBorder(Color.WHITE, 1)); break;
+				this.setBorder(BorderFactory.createLineBorder(Color.ORANGE, 1)); break;
 		}
 
 		// Add avatar label regardless of if one exists just to have UI matching
